ed05532e566ec8efbfe2b86118ccd0a2e9c4e2be
wujun <8400684@qq.com>
2023-02-18 14:39:19 +0800

    ini

---
+++ ExpressionDemo/Message.cs
@@ -0,0 +1,62 @@
+namespace ExpressionDemo;
+
+public interface IMessage<out T> : IMessage
+{
+    /// <summary>
+    /// The message body as a .NET type.
+    /// This will return the same underlying object than <see cref="IMessage.GetBody"/> but will be strongly typed.
+    /// </summary>
+    T? Body { get; }
+}
+
+public interface IMessage
+{
+    /// <summary>
+    /// The message properties.
+    /// </summary>
+    MessageProperties Properties { get; }
+
+    /// <summary>
+    /// The message body return as an object when we only have runtime types and can't use generics.
+    /// </summary>
+    object? GetBody();
+
+    /// <summary>
+    /// The message <see cref="Type"/>. This is a shortcut to GetBody().GetType().
+    /// </summary>
+    Type MessageType { get; }
+}
+
+public class Message<T>  : IMessage
+{
+    public MessageProperties Properties { get; }
+    public Type MessageType { get; }
+    public T? Body { get; }
+
+    public object? GetBody() { return Body; }
+
+    public Message(T? body)
+    {
+        Body = body;
+        Properties = new MessageProperties();
+        MessageType = body != null ? body.GetType() : typeof(T);
+    }
+
+    public Message()
+    {
+        Body = default;
+        Properties = default;
+        MessageType = typeof(T);
+    }
+
+    public Message(T? body, in MessageProperties properties)
+    {
+        Body = body;
+        Properties = properties;
+        MessageType = body != null ? body.GetType() : typeof(T);
+    }
+}
+
+public class MessageProperties
+{
+}
